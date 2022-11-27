7e3e19ea46cd1cab3810028f5116f7cefa21f707
Red Riot <itxpapon@gmail.com>
2022-11-27 14:12:49 +0600

    first upload

---
+++ app/src/main/java/com/example/SignDecoder/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.SignDecoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends Fragment {
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d("MainActivity: ","Opencv is loaded");
+        }
+        else {
+            Log.d("MainActivity: ","Opencv failed to load");
+        }
+    }
+
+
+    private Button camera_button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+
+        camera_button=view.findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/SignDecoder/home.java
@@ -0,0 +1,57 @@
+package com.example.SignDecoder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+public class home extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ViewPager   viewPager;
+   // home homeFragment = new home();
+    MainActivity signtoletter = new MainActivity();
+    frag1 homeFragment = new frag1();
+    frag2 signtoword = new frag2();
+    frag3 signtospeech = new frag3();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        bottomNavigationView  = findViewById(R.id.bottomNavbar);
+        viewPager = findViewById(R.id.viewpager);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,homeFragment).commit();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,homeFragment).commit();
+                        return true;
+                    case R.id.letter:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,signtoletter).commit();
+                        return true;
+                    case R.id.word:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,signtoword).commit();
+                        return true;
+                    case R.id.voice:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,signtospeech).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+
+    }
+}
