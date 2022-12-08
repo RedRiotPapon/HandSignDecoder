fd2fc200f8f011c340578f69b931f0e949b00ee6
Red Riot <itxpapon@gmail.com>
2022-12-09 00:51:04 +0600

    commit

--- app/src/main/java/com/example/SignDecoder/MainActivity.java
+++ app/src/main/java/com/example/SignDecoder/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.SignDecoder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +29,8 @@ public class MainActivity extends Fragment {
 
 
     private Button camera_button;
+    private Button bgbtn1;
+    private Button bgbtn2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,6 +39,27 @@ public class MainActivity extends Fragment {
         View view = inflater.inflate(R.layout.activity_main, container, false);
 
         camera_button=view.findViewById(R.id.camera_button);
+        bgbtn1=view.findViewById(R.id.bgbtn);
+        bgbtn2=view.findViewById(R.id.bgbtn2);
+        ObjectAnimator scaleDown = ObjectAnimator.ofPropertyValuesHolder(
+                bgbtn1,
+                PropertyValuesHolder.ofFloat("scaleX", 1.25f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.25f));
+        scaleDown.setDuration(310);
+        scaleDown.setInterpolator(new FastOutSlowInInterpolator());
+        scaleDown.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown.setRepeatMode(ObjectAnimator.REVERSE);
+
+        ObjectAnimator scaleDown1 = ObjectAnimator.ofPropertyValuesHolder(
+                bgbtn2,
+                PropertyValuesHolder.ofFloat("scaleX", 1.5f),
+                PropertyValuesHolder.ofFloat("scaleY", 1.5f));
+        scaleDown1.setDuration(310);
+        scaleDown1.setInterpolator(new FastOutSlowInInterpolator());
+        scaleDown1.setRepeatCount(ObjectAnimator.INFINITE);
+        scaleDown1.setRepeatMode(ObjectAnimator.REVERSE);
+        scaleDown1.start();
+        scaleDown.start();
         camera_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
